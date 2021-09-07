12f85a133b49737ef60367d35f3fd530c0842115
Hanno <h.hildenbrandt@rug.nl>
2021-09-07 18:39:55 +0200

    initial commit
    Signed-off-by: Hanno <h.hildenbrandt@rug.nl>

---
+++ README.md
@@ -0,0 +1,11 @@
+# UWP visual studio code setup
+
+## Prerequisites 
+
+
+## Install msys on the X-drive
+
+Download msys2 https://github.com/msys2/msys2-installer/releases/download/2021-07-25/msys2-base-x86_64-20210725.sfx.exe
+
+Run `setup.bat` from this repository. Accept all default choices and igrnore warnings.
+The command will take a long time (read: ages) if you are on uwp.rug.nl - grab a coffee and file a complaint with ITC over the poor uplink.
---
+++ setup.bat
@@ -0,0 +1,57 @@
+@echo off
+
+:: install script for vscode + msys2
+:: Hanno 2021
+
+setlocal
+SET INSTALL_DIR=X:
+SET PROJECT_DIR=%INSTALL_DIR%\vscode_projects"
+
+SET MSYS=%INSTALL_DIR%/msys64
+SET PATH=%MSYS%/usr/bin/;%MSYS%/mingw64/bin/;%INSTALL_DIR%\vscode\bin\;%PATH%
+
+:: msys2
+call :update_pacman_repositories
+call :update_msys
+call :install_develop_shebang
+pkgfile --update
+
+:: vscode
+call :fetch_vscode
+call :install_code_extension ms-vscode.cmake-tools
+call :install_code_extension ms-vscode.cpptools
+:: call :install_code_extension fougas.msys2
+:: call :install_code_extension shd101wyy.markdown-preview-enhanced
+call :create_project_dir
+exit /B %ERRORLEVEL%
+
+:update_pacman_repositories
+:: check disk space seg-faults on UWP - disable it
+sed -i 's/^CheckSpace/#CheckSpace/g' %MSYS%/etc/pacman.conf
+pacman --noconfirm -Suy
+exit /B 0
+
+:update_msys
+pacman --noconfirm -Su
+exit /B 0
+
+:install_develop_shebang
+pacman -S unzip git mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja
+exit /B 0
+
+:fetch_vscode
+wget "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive" -O vscode.zip
+unzip vscode.zip -d %INSTALL_DIR%\vscode
+del vscode.zip
+exit /B 0
+
+:create_project_dir
+mkdir %PROJECT_DIR%
+echo SET PATH=%MSYS%/mingw64/bin/;%INSTALL_DIR%\vscode\bin\;%%PATH%% > %PROJECT_DIR%\launch_vscode.bat
+echo code --disable-gpu . >> %PROJECT_DIR%\launch_vscode.bat
+exit /B 0
+
+:install_code_extension
+echo installing %~1
+call code --disable-gpu --install-extension %~1 --force
+exit /B 0
