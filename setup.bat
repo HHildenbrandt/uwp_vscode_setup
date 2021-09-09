ff498a5b3a0914742c3cbc505f23f8d142feaf13
Hanno <h.hildenbrandt@rug.nl>
2021-09-09 15:40:29 +0200

    lldb-mi build from source
    Signed-off-by: Hanno <h.hildenbrandt@rug.nl>

--- setup.bat
+++ setup.bat
@@ -7,19 +7,18 @@ setlocal
 
 SET CWD=%~dp0
 SET INSTALL_DIR=X:
-
 IF "%~1" == "" GOTO :USE_DEFAULT_INSTALL_DIR
 SET INSTALL_DIR=%1
 :USE_DEFAULT_INSTALL_DIR
 
 SET PROJECT_DIR=%INSTALL_DIR%\vscode_projects
-
 IF "%~2" == "" GOTO :USE_DEFAULT_PROJECT_DIR
 SET PROJECT_DIR=$2
 :USE_DEFAULT_PROJECT_DIR
 
+SET CRT=mingw64
 SET MSYS=%INSTALL_DIR%/msys64
-SET PATH=%MSYS%/usr/bin/;%MSYS%/mingw64/bin/;%INSTALL_DIR%\vscode\bin\;%PATH%
+SET PATH=%MSYS%\usr\bin\;%MSYS%\%CRT%\bin\;%INSTALL_DIR%\vscode\bin\;%PATH%
 
 :: msys2
 call :update_pacman_repositories
@@ -33,6 +32,7 @@ call :install_code_extension ms-vscode.cpptools
 :: call :install_code_extension fougas.msys2
 call :install_code_extension shd101wyy.markdown-preview-enhanced
 call :create_project_dir
+call :build_lldb-mi
 exit /B %ERRORLEVEL%
 
 :update_pacman_repositories
@@ -46,7 +46,7 @@ pacman --noconfirm -Su
 exit /B 0
 
 :install_develop_shebang
-pacman -S unzip git mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja
+pacman -S unzip git mingw-w64-x86_64-toolchain mingw-w64-x86_64-clang mingw-w64-x86_64-lldb mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja mingw-w64-x86_64-polly
 exit /B 0
 
 :fetch_vscode
@@ -62,8 +62,20 @@ exit /B 0
 
 :create_project_dir
 mkdir %PROJECT_DIR%
-echo SET PATH=%MSYS%/mingw64/bin/;%INSTALL_DIR%\vscode\bin\;%%PATH%% > %PROJECT_DIR%\launch_vscode.bat
+echo SET PATH=%MSYS%/%CRT%/bin/;%INSTALL_DIR%\vscode\bin\;%%PATH%% > %PROJECT_DIR%\launch_vscode.bat
 echo code %DISABLEGPU% . >> %PROJECT_DIR%\launch_vscode.bat
 echo %CWD%cmake_project %PROJECT_DIR%\cmake-project
 xcopy /E /I %CWD%cmake-project %PROJECT_DIR%\cmake-project
+exit /B 0
+
+:build_lldb-mi
+wget "https://github.com/lldb-tools/lldb-mi/archive/refs/heads/master.zip" -O lldb-mi.zip
+unzip lldb-mi.zip -d lldb-mi
+pushd lldb-mi\lldb-mi-master
+cmake .
+cmake --build .
+cp src\lldb-mi.exe %MSYS%\%CRT%\bin\
+popd
+rmdir /s lldb-mi
+del lldb-mi.zip
 exit /B 0
